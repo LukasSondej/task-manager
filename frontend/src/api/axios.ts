f2664f84c1d5e7c0a2711da2b0278d6bcdba620f
Łukasz Sondej <sondejlukasz0@gmail.com>
2026-04-01 08:50:25 +0200

    feat: add Axios response interceptor for 401/403

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -4,12 +4,21 @@ const instance = axios.create({
 });
 instance.interceptors.request.use(function (config) {
 if(localStorage.getItem("accessToken")){
-    config.headers.Authorization = "Bearer " + localStorage.getItem("accessToken")
+  config.headers.Authorization = "Bearer "+ localStorage.getItem("accessToken")
 }
     return config;
   }, function (error) {
     return Promise.reject(error);
   },
 );
+instance.interceptors.response.use(function onFulfilled(response) {
 
+    return response;
+  }, function onRejected(error) {
+if(error.response && error.response.status == 401 || error.response.status === 403){
+  localStorage.removeItem("accessToken")
+  window.location.href ="/login"
+}
+    return Promise.reject(error);
+  });
 export default instance
