2e7989d8b373ffe90d039e0cd7fdf21363d25a6b
Łukasz Sondej <sondejlukasz0@gmail.com>
2026-04-11 10:28:42 +0200

    feat: add task filtering by status

--- frontend/src/pages/TasksPage.tsx
+++ frontend/src/pages/TasksPage.tsx
@@ -7,6 +7,7 @@ import { TaskForm } from "@/components/TaskForm"
 import type { taskType } from "../../../backend/schemas"
 import { TaskCard } from "@/components/TaskCard"
 import { NavBar } from "@/components/NavBar"
+import { Button } from "@/components/ui/button"
 
 
 type Task = {
@@ -16,6 +17,7 @@ type Task = {
     status: string
 }
 export const TasksPage =()=>{
+    const [filter , setFilter] = useState("ALL")
     const [isFormOpen, setIsFormOpen] = useState(false);
 const dispatch = useDispatch<AppDispatch>()
 
@@ -30,17 +32,24 @@ const onSubmit = (data: taskType) => {
   setIsFormOpen(false); 
 }
 
+const handleChangeStatusTasks = (status: string) => {
+setFilter(status)
+}
 
 if(isLoading) return <p>Loading...</p>
 if(isError) return <p>Error</p>
-
+const filteredTasks = items.filter(task => filter === "ALL" || task.status === filter)
     return (<div>
         <NavBar/>
 <div>Tasks!!! </div>
 <div><button onClick={()=>setIsFormOpen((prev) => !prev)}>Add task</button></div>
 {isFormOpen && <TaskForm onSubmit={onSubmit} onClose={() => setIsFormOpen(false)}/>}
+    <Button variant={filter === "ALL" ? "default" : "outline"} onClick={() => handleChangeStatusTasks("ALL")}>ALL</Button>
+    <Button variant={filter === "TODO" ? "default" : "outline"} onClick={() => handleChangeStatusTasks("TODO")}>TODO</Button>
+    <Button variant={filter === "IN_PROGRESS" ? "default" : "outline"} onClick={() => handleChangeStatusTasks("IN_PROGRESS")}>IN_PROGRESS</Button>
+    <Button variant={filter === "DONE" ? "default" : "outline"} onClick={() => handleChangeStatusTasks("DONE")}>DONE</Button>
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-        {items.map((task: Task) => <TaskCard key={task.id} task={task}/>)}
+        {filteredTasks.map((task: Task) => <TaskCard key={task.id} task={task}/>)}
     </div>
 
 <Link to={"/login"}>Login Page</Link>
